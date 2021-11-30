0dab8350298673c3e422c701d4cb81b60a23385c
T.J. Daddio <cheery.home1669@fastmail.com>
2021-11-29 20:51:50 -0500

    Initial Commit

---
+++ sqlcuts/sqlcutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  sqlcutsApp.swift
+//  sqlcuts
+//
+//  Created by Tyler James Daddio on 11/29/21.
+//
+
+import SwiftUI
+
+@main
+struct sqlcutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
